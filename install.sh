fe311659543ff0b0c1be530b9762cd1ffcf69323
Alex Lowe <loweman64@outlook.com>
2023-03-13 23:35:12 -0400

    feat: Encrypt 3rd Partition and Open

--- install.sh
+++ install.sh
@@ -15,11 +15,27 @@ do
 	let "index+=1"
 done
 
-echo -n "Enter Something: "
-read answer
-diskPath=${fdiskSearchArray[answer-1]}
+echo -n "Select Number Corrisponding to the Desired Path: "
+read diskPathAnswer
+diskPath=${fdiskSearchArray[diskPathAnswer-1]}
 
-sed -r "s|DISK_PATH|${diskPath}|" create-partition.exp.template > create-partition.exp
+requestUserPassword() {
+	read -sp "Enter Encryption Password: " encryptionPassword
+	printf "\n"
+	read -sp "Retype Password: " encryptionPasswordConfirm
+	if [[ -z $encryptionPassword ]] || [[ $encryptionPassword != $encryptionPasswordConfirm ]]; then
+		return 1
+	fi
+	return 0
+}
+
+requestUserPassword
+while [ $? -ne 0 ]; do
+	echo "Sorry the passwords you entered does not match"
+	requestUserPassword
+done
+
+sed -sr 's|''\$DISK_PATH''|'"$diskPath|g" ./templates/create-partition.exp.template > create-partition.exp
 chmod +x create-partition.exp
 ./create-partition.exp
 
@@ -29,4 +45,17 @@ listAllDisksCMD=$(echo "$listAllDisksTemplate" | sed "s|DISK_PATH|$diskPath|g")
 readarray -t listAllDisksArray <<< $(eval "$listAllDisksCMD")
 echo "These partitions were created: ${listAllDisksArray[@]}"
 
+encryptionPartition=${listAllDisksArray[-1]}
+
+# Encrypt Data Partition
+sed -sr -e 's|''\$DISK_PATH''|'"${encryptionPartition}|g" -e 's|''\$PASSWORD''|'"${encryptionPassword}|g" ./templates/encrypt-partition.exp.template > encrypt-partition.exp
+chmod +x encrypt-partition.exp
+./encrypt-partition.exp
+
+# Open Encrypted Partition
+sed -sr -e 's|''\$DISK_PATH''|'"${encryptionPartition}|g" -e 's|''\$PASSWORD''|'"${encryptionPassword}|g" ./templates/decrypt-partition.exp.template > decrypt-partition.exp
+chmod +x decrypt-partition.exp
+./decrypt-partition.exp
+
+# Clean up
 rm create-partition.exp
